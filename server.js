12af42f7764e04c149e3fab1bdeeb3fc645bd361
Alexis Burgos (gtm) <alexisburgosgtm@outlook.com>
2021-09-21 20:27:27 -0600

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ router.use(function (req,res,next) {
 // Configurar cabeceras y cors
 app.use((req, res, next) => {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // Request headers you wish to allow
