dd5ab9a250e477650622f58c17a5d32bcf71f5bf
Alexis Burgos (gtm) <alexisburgosgtm@outlook.com>
2021-09-27 09:17:42 -0600

    Update server.js

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ var path = __dirname + '/'
 app.use(function (req,res,next) {
       // Website you wish to allow to connect
       res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
+      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
       res.setHeader('Access-Control-Allow-Origin', 'https://business-intelligence-mercados.herokuapp.com');
       
       // Request methods you wish to allow
