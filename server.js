d54c26f7fd2f4dfcc6a356e8aef9c73fa9f7d7d7
Alexis Burgos (gtm) <alexisburgosgtm@outlook.com>
2021-09-27 08:29:57 -0600

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var path = __dirname + '/'
 router.use(function (req,res,next) {
   
       // Website you wish to allow to connect
-      res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:3000, https://business-intelligence-mercados.herokuapp.com/');
+      res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:3000, https://business-intelligence-mercados.herokuapp.com');
       // Request methods you wish to allow
       res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
         // Request headers you wish to allow
