7cdbadb8de30d74e2940760a982c962c6d4f1f50
Alexis Burgos (gtm) <alexisburgosgtm@outlook.com>
2021-09-27 03:02:37 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var router = express.Router();
 var bodyParser = require('body-parser');
-const cors = require('cors');
+//var cors = require('cors');
 
 //const execute = require('./router/connection');
 //const { send } = require("process");
@@ -21,43 +21,53 @@ app.use(express.static('build'));
 var path = __dirname + '/'
 
 //manejador de rutas
-router.use(
-  cors({
-        origin: '*',
-        method: ['POST','GET']
-      })
- );
+router.use((res,req,next)=>{
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://business-intelligence-mercados.herokuapp.com/');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
+    // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name, pplication/json');
+    // Set to true if you need the website to include cookies in the requests sent
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  next();
+});
 
 // Configurar cabeceras y cors
-app.use(
+app.use((res,req,next)=>{
   // Website you wish to allow to connect
-  //res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://business-intelligence-mercados.herokuapp.com/');
   // Request methods you wish to allow
-  //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
     // Request headers you wish to allow
-  //res.setHeader('Access-Control-Allow-Headers', 'Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name, pplication/json');
+    // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name, pplication/json');
     // Set to true if you need the website to include cookies in the requests sent
-  //res.setHeader('Access-Control-Allow-Credentials', true);
-  cors({
-    origin: '*',
-    method: ['GET','POST']
-  })
-  //next();
-);
+  res.setHeader('Access-Control-Allow-Credentials', true);
+ 
+
+  next();
+});
+
 
 app.use("/",router);
 
 app.use("*",function(req,res){
   //res.redirect('/');
-  //res.send('<h1 class="text-danger">NO DISPONIBLE</h1>');
-	res.sendFile(path + 'favicon.png');
+  res.send('<h1 class="text-danger">NO DISPONIBLE</h1>');
+	//res.sendFile(path + 'favicon.png');
 });
 
 app.get("/",function(req,res){
   //execute.start();
-	res.sendFile(path + 'favicon.png');
+	//res.sendFile(path + 'favicon.png');
  
-  //res.send('<h1>Acceso Restringido</h1>')
+  res.send('<h1>Mercados Efectivos</h1>')
 }); 
 
 
@@ -65,11 +75,6 @@ app.get("/",function(req,res){
 app.use('/marcas', routerMarcas);
 
 app.use('/empresas', routerEmpresas);
-
-
-
-
-
 
 // SOCKET HANDLER
 io.on('connection', function(socket){
