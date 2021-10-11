02ae586b4f8a2e5b7b0c16f32ba3c987d814257b
Alexis Burgos (gtm) <alexisburgosgtm@outlook.com>
2021-10-11 00:57:23 -0600

    charts

--- build/views/viewInicio.js
+++ build/views/viewInicio.js
@@ -8,11 +8,11 @@ function getView(){
                 </div>
 
                 <div class="col-sm-12 col-lg-4 col-xl-4 col-md-4 card shadow border-top-rounded border-bottom-rounded">
-                    <canvas id="myChart2" width="50" height="50"></canvas>
+                    <canvas id="myChart3" width="50" height="50"></canvas>
                 </div>
 
                 <div class="col-sm-12 col-lg-4 col-xl-4 col-md-4 card shadow border-top-rounded border-bottom-rounded">
-                    <canvas id="myChart" width="50" height="50"></canvas>
+                    <canvas id="myChart2" width="50" height="50"></canvas>
                 </div>
             </div>
             `
@@ -20,7 +20,9 @@ function getView(){
         listado:()=>{
             return `
             <div class="row">
-                tabla
+                <div class="table-responsive" id="tblTabla">
+                
+                </div>
             </div>
             `
         }
@@ -33,9 +35,14 @@ function addListeners(){
 
     getDataResumen()
     .then((empresas)=>{
-      
-        getPieChar(empresas);
-        getBarChar(empresas);
+        getPieCharVentas(empresas);
+        getPieCharDevoluciones(empresas);
+        getBarCharUtilidades(empresas);
+    })
+
+    getDataFechas()
+    .then((datos)=>{
+        getTblVentasFecha(datos)
     })
   
 };
@@ -66,7 +73,30 @@ function getDataResumen(){
 
 };
 
-function getPieChar(data){
+function getDataFechas(){
+
+    return new Promise((resolve, reject)=>{
+        let anio = 2021; let mes = 9;
+        //obtiene los datos de la card empresas
+        let str = `'ME-IZABAL','ME-PETEN','ME-ZACAPA','ME-JUTIAPA','ME-COBAN'`;
+        axios.get(`/empresas/getVentasFechaEmpresas?filtro=${str}&anio=${anio}&mes=${mes}`)
+        .then(res => {
+            const datos = res.data.recordset;
+           
+            resolve(datos);
+        })
+        .catch(()=>{
+            reject();
+        })
+
+
+    })
+     
+
+};
+
+
+function getPieCharVentas(data){
    
 
     let label = []; let valor = []; let bgColor = [];
@@ -106,7 +136,7 @@ function getPieChar(data){
                   },
                   title: {
                     display: true,
-                    text: 'Ventas por Sede'
+                    text: 'VENTAS por Sede'
                   },
                 // Change options for ALL labels of THIS CHART
                 datalabels: {
@@ -142,7 +172,83 @@ function getPieChar(data){
 
 };
 
-function getBarChar(data){
+function getPieCharDevoluciones(data){
+   
+
+    let label = []; let valor = []; let bgColor = [];
+    let total = 0;
+    data.map((r)=>{
+        total = total + Number(r.DEVOLUCIONES);
+    });
+   
+    data.map((r)=>{
+            label.push(r.EMPNIT);
+            valor.push( Number((Number(r.DEVOLUCIONES)/total).toFixed(2))*100);
+            bgColor.push(getRandomColor())
+    })
+
+  
+    var ctx = document.getElementById('myChart3').getContext('2d');
+    var myChart = new Chart(ctx, {
+        plugins: [ChartDataLabels],
+        type: 'doughnut',
+        data: {
+            labels: label,
+            datasets: [{
+                data:valor,
+                borderColor: 'white',
+                backgroundColor:bgColor
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            },
+            plugins: {
+                legend: {
+                    position: 'top',
+                  },
+                  title: {
+                    display: true,
+                    text: 'DEVOLUCIONES por Sede'
+                  },
+                // Change options for ALL labels of THIS CHART
+                datalabels: {
+                  anchor:'end',
+                  align:'end',
+                  listeners: {
+                    click: function(context) {
+                      // Receives `click` events only for labels of the first dataset.
+                      // The clicked label index is available in `context.dataIndex`.
+                      console.log(context);
+                    }
+                  },
+                  formatter: function(value) {
+                    return value + '%';
+                    // eq. return ['line1', 'line2', value]
+                  },
+                  color: function(context) {
+                    return context.dataset.backgroundColor;
+                  },
+                  borderColor: 'white',
+                  borderRadius: 25,
+                  borderWidth: 0,
+                  font: {
+                    weight: 'bold'
+                  }
+                }
+            }
+        }
+    });
+
+
+    
+
+};
+
+function getBarCharUtilidades(data){
    
 
     let label = []; let valor = []; let bgColor = [];
@@ -218,6 +324,81 @@ function getBarChar(data){
 
 };
 
+function getLineChartFechas(data){
+   
+
+    let label = []; let valor = []; let bgColor = [];
+    let total = 0;
+    data.map((r)=>{
+        total = total + Number(r.UTILIDAD);
+    });
+   
+    data.map((r)=>{
+            label.push(r.EMPNIT);
+            valor.push( Number(r.UTILIDAD).toFixed(2));
+            bgColor.push(getRandomColor())
+    })
+
+
+    var ctx = document.getElementById('myChart2').getContext('2d');
+    var myChart = new Chart(ctx, {
+        plugins: [ChartDataLabels],
+        type: 'line',
+        data: {
+            labels: label,
+            datasets: [{
+                data:valor,
+                borderColor: 'white',
+                backgroundColor:bgColor
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            },
+            plugins: {
+                legend: {
+                    position: 'top',
+                  },
+                  title: {
+                    display: true,
+                    text: 'Ventas por Fecha por Sede: ' + funciones.setMoneda(total,'Q')
+                  },
+                // Change options for ALL labels of THIS CHART
+                datalabels: {
+                  anchor:'end',
+                  align:'end',
+                  listeners: {
+                    click: function(context) {
+                      // Receives `click` events only for labels of the first dataset.
+                      // The clicked label index is available in `context.dataIndex`.
+                      console.log(context);
+                    }
+                  },
+                  formatter: function(value) {
+                    return 'Q' + value;
+                    // eq. return ['line1', 'line2', value]
+                  },
+                  color: function(context) {
+                    return context.dataset.backgroundColor;
+                  },
+                  borderColor: 'white',
+                  borderRadius: 25,
+                  borderWidth: 0,
+                  font: {
+                    weight: 'bold'
+                  }
+                }
+            }
+        }
+    });
+
+
+    
+
+};
 
 function getRandomColor() {
     var letters = '0123456789ABCDEF'.split('');
@@ -226,4 +407,43 @@ function getRandomColor() {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
+};
+
+
+function getTblVentasFecha(data){
+    
+    let container = document.createElement('tblTabla');
+    container.innerHTML = getLoader();
+
+    let head = `<table class="table table-responsive table-hover table-striped>
+                    <thead>
+                        <tr>
+                            <td>FECHA</td>
+                            <td>SUCURSAL</td>
+                            <td>COSTO</td>
+                            <td>VENTA</td>
+                            <td>UTILIDAD</td>
+                        </tr>
+                    </thead>
+                    <tbody>`;
+
+    let foot = `</tbody></table>`
+
+    let dat = '';
+
+    data.map((r)=>{
+        dat += `
+            <tr>
+                <td>${funciones.cleanFecha(r.FECHA)}</td>
+                <td>${r.CODSUCURSAL}</td>
+                <td>${funciones.setMoneda(r.TOTALCOSTO,'Q')}</td>
+                <td>${funciones.setMoneda(r.TOTALPRECIO,'Q')}</td>
+                <td>${funciones.setMoneda(r.TOTALUTILIDAD,'Q')}</td>
+            </tr>
+        `
+    })
+
+    container.innerHTML = head + dat + foot
+
+
 }
--- router/routerEmpresas.js
+++ router/routerEmpresas.js
@@ -3,12 +3,30 @@ const execute = require('./connection');
 const express = require('express');
 const router = express.Router();
 
+router.get('/getVentasFechaEmpresas', async function(req,res){
+
+    const {anio,mes,filtro} = req.query;
+
+    filtro = `'ME-ZACAPA', 'ME-PETEN', 'ME-COBAN', 'ME-IZABAL', 'ME-JUTIAPA'`;
+
+    let qry = `SELECT        CODSUCURSAL, FECHA, 
+                SUM(TOTALCOSTO) AS TOTALCOSTO, 
+                SUM(TOTALPRECIO) AS TOTALPRECIO, 
+                (SUM(TOTALPRECIO) - SUM(TOTALCOSTO)) AS TOTALUTILIDAD 
+    FROM            BI_RPT_GENERAL
+    GROUP BY CODSUCURSAL, FECHA, ANIO, MES
+    HAVING        (CODSUCURSAL IN (${filtro})) AND (ANIO = ${anio}) AND (MES = ${mes})`;
+
+    execute.Query(res,qry);
+
+});
 
 router.get('/getfacdevempresas', async function(req,res){
 
     const {anio,mes} = req.query;
 
-    let qry = `SELECT        CODSUCURSAL, TIPO, SUM(TOTALPRECIO) AS TOTALPRECIO
+    let qry = `SELECT 
+        CODSUCURSAL, TIPO, SUM(TOTALPRECIO) AS TOTALPRECIO
     FROM            BI_RPT_GENERAL
     WHERE        (ANIO = ${anio}) AND (MES = ${mes})
     GROUP BY CODSUCURSAL, TIPO
@@ -21,7 +39,14 @@ router.get('/getfacdevempresas', async function(req,res){
 router.get("/getempresas", async function(req,res){
     const {empresas} = req.query;
 
-    let qry = `SELECT EMPNIT, NOMBRE, VENTAS,COSTO,UTILIDAD,MARGEN,OBJETIVO, UNIVERSO FROM BI_EMPRESAS_RESUMEN WHERE EMPNIT IN(${empresas})`
+    let qry = `SELECT EMPNIT, NOMBRE, 
+                COSTO,VENTAS,
+                COSTODEV,DEVOLUCIONES,
+                UTILIDAD,MARGEN,OBJETIVO, 
+                UNIVERSO 
+                FROM BI_EMPRESAS_RESUMEN 
+                WHERE EMPNIT IN(${empresas})`
+    
     execute.Query(res,qry);
 });
 
